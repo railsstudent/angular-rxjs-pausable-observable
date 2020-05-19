95bdfb2ba375fe83f3405ece2995b8403ac4a3ee
railsstudent <cleung334@gmail.com>
2020-05-19 14:54:18 +0800

    takeUntil to unsubscribe observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { fromEvent, merge, EMPTY } from 'rxjs';
-import { tap, takeUntil, mapTo, switchMap, startWith } from 'rxjs/operators';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { fromEvent, merge, EMPTY, Subject } from 'rxjs';
+import { tap, mapTo, switchMap, startWith, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent implements OnInit {
-  name = 'Angular';
+export class AppComponent implements OnInit, OnDestroy {
+  private unsubscribe$ = new Subject();
 
   ngOnInit() {
     const send$ = fromEvent(document.getElementById('send'), 'click');
@@ -25,7 +25,13 @@ export class AppComponent implements OnInit {
         switchMap(v => 
           (v === 'stop') ? EMPTY : logging$
         ),
+        takeUntil(this.unsubscribe$)
       )
       .subscribe();
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
